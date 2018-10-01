e9b91a77ed4e94c59477aff0cd0fe6cafe96f236
Rafael Carrascal R <rcarrascal@gmail.com>
2018-09-30 21:05:01 -0500

    angular 6 with back4app basic config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {environment} from '../environments/environment';
+
+declare const Parse: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'celestial-front';
+  email = '';
+  password = '';
+  rememberMe = false;
+
+  constructor() {
+    Parse.initialize(environment.PARSE_APP_ID, environment.PARSE_JS_KEY);
+    Parse.serverURL = environment.serverURL;
+  }
+
+  handleSubmit = () => {
+    const user = new Parse.User();
+    user.set('username', this.email);
+    user.set('email', this.email);
+    user.set('password', this.password);
+    user.set('rememberMe', this.rememberMe);
+
+    user.signUp(null).then(
+        function(user) {
+            alert('User created successfully with email: ' + user.get('email'));
+        },
+
+        function(error) {
+            alert('Error ' + error.code + ':' + error.message);
+        }
+    );
+  }
+
+  handleUsernameChange = (event: KeyboardEvent) => {
+      this.email = (<HTMLInputElement>event.target).value;
+  }
+
+  handlePasswordChange = (event: KeyboardEvent) => {
+      this.password = (<HTMLInputElement>event.target).value;
+  }
+
+  handleRememberMeClick = () => {
+      this.rememberMe = !this.rememberMe;
+  }
 }
